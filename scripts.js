5758c5e0bd92a18f8210855ee4a4b4bbd80a915d
Vinícius Zanotti de Oliveira <130515950+vinizanotti89@users.noreply.github.com>
2024-11-02 00:41:39 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
-// - Variável - Uma parte a memória do computador para guardar o que quiser
-// - Funções - Pedaço de código que só executa quando é chamado
-// - DOCUMENT - HTML
-// - querySelector - (Selecionar ou pegar um item do HTML)
-//addEventListener - Vizinha fofoqueira (Ouvindo alguém e te connta quando algo acontecer)
+
 
 let botaoSom = document.querySelector(".botao-som")
 let video = document.querySelector(".video")
@@ -30,7 +26,7 @@ function esconderModal(){
     modal.style.display = "none"
 }
 
-// Window - site
+
 
 window.addEventListener("load", tocarAudio)
 
